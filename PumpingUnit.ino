3caaaeab06e8d8e1b2d6c9b1ec01ecd0de7d0b93
Duysburg <66062396+Duysburg@users.noreply.github.com>
2024-01-16 12:17:31 +0100

    first Pumping Unit

--- PumpingUnit.ino
+++ PumpingUnit.ino
@@ -1,1 +1,27 @@
+class Pump {
+  public:
+    Pump(int pumpPin) {
+      this->PumpPin = pumpPin;
+    }
 
+    /// the pin the physical pump is connected to
+    int PumpPin;
+
+    void setPumpActivity(bool pumpActivity) {
+      digitalWrite(PumpPin, pumpActivity ? HIGH : LOW);
+    }
+
+    void pumpForTime(unsigned long timeMillis) {
+      // start pumping
+      unsigned long startTime = millis();
+      digitalWrite(pumpPin, HIGH);
+
+      // wait until time the program should pump has run out
+      while (millis() - startTime < timeMillis) {
+        // wait
+      }
+
+      // stop pumping
+      digitalWrite(pumpPin, LOW);
+    }
+};
