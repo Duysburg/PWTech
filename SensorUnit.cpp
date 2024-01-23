8b4300137fde57339106ffc0c663c73bdb4daaf2
Happy <105150601+hpyGithub@users.noreply.github.com>
2024-01-23 19:07:07 +0100

    Duysburg: Update SensorUnit.cpp

--- SensorUnit.cpp
+++ SensorUnit.cpp
@@ -1,24 +1,34 @@
-
 //class definition
-struct _Sensor{
-    
-    int powerpin;         //PowerPin
+class Sensor{
+
+    private: 
+    int powerPin;         //PowerPin
+    int readingPin;       //ReadingPin
+
+    public: 
     int actual_moisture;  //sensor value of moisture 
 
-    Sensor(int pumpPin) {
-      this->PumpPin = pumpPin;
+    Sensor(int powerP, int readP) {
+      this->powerPin = powerP;
+      this->readingPin = readP; 
+      this->actual_moisture = moistureSensor(); 
     }
 
-    void setMoistureSensor(){//sensor measuring
-        digitalWrite(powerpin, HIGH);
+    int moistureSensor(){//sensor measuring
+        digitalWrite(powerPin, HIGH);
         delay(10);
-        this->actual_moisture = analogRead(A0);  //read sensor
-        digital.Write(powerpin, LOW);
+        this->actual_moisture = analogRead(readingPin);  //read sensor
+        digitalWrite(powerPin, LOW);
+        return analogRead(readingPin); 
     }
 
-} Sensor;
+};
 
-  
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin(9600);
 
 //
 //not implemented yet 
@@ -28,6 +38,7 @@ struct _Sensor{
   //lightSwitch(sensor.actual_moisture);
 
 //LightFunction - not implemented
+/*
 void lightSwitch(int sensor, Plant p){
   if(sensor > p->thresh){
     digitalWrite(redLED, LOW); 
@@ -37,3 +48,20 @@ void lightSwitch(int sensor, Plant p){
     digitalWrite(greenLED, LOW); 
   }
 };
+*/
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  Sensor sensor = Sensor(8, A0); 
+  Serial.println(sensor.actual_moisture);
+  Serial.println("running ..."); 
+
+  Serial.println(sensor.actual_moisture);
+  Serial.println("Running ..."); 
+
+  delay(1000); 
+
+}
