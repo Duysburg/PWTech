60dcd807304a20f3bf75c12f46f96fe5472588ec
Happy <connect1234567890@web.de>
2024-01-23 11:55:15 +0100

    cleanup, documentation and rename for clarity

---
+++ PumpingUnit.h
@@ -0,0 +1,36 @@
+class Pump {
+  public:
+    
+    /// the pin the physical pump is connected to
+    int PumpPin;
+
+    Pump(int pumpPin) {
+      PumpPin = pumpPin;
+    }
+
+    /**
+     * sets the pump activity based on the given boolean value
+     * @param pumpActivity the boolean value indicating whether the pump should be active or not
+     */
+    void setPumpActivity(bool pumpActivity) {
+      digitalWrite(PumpPin, pumpActivity ? HIGH : LOW);
+    }
+
+    /**
+     * pumps the pump for a given time
+     * @param timeMillis the time in milliseconds that the pump should run for
+     */
+    void pumpForTime(unsigned long timeMillis) {
+      // start pumping
+      unsigned long startTime = millis();
+      digitalWrite(pumpPin, HIGH);
+
+      // wait until time the program should pump has run out
+      while (millis() - startTime < timeMillis) {
+        // wait
+      }
+
+      // stop pumping
+      digitalWrite(pumpPin, LOW);
+    }
+};
