926b576ac97399677df07cd801293d299ad3428e
Happy <connect1234567890@web.de>
2024-01-25 11:46:18 +0100

    added RobotBrain class and additional documentation

--- RobotBrain/RobotBrain.ino
+++ RobotBrain/RobotBrain.ino
@@ -4,20 +4,52 @@
 void setup()
 {
     Serial.begin(9600);
-    Serial.println("Power to pin!");
-    TEST_Pump(7);
-    TEST_Sensor(8, A0);
+    Serial.println("Robot started!");
+
+    RobotBrain robotBrain = RobotBrain(pumpList, durationList);
+    robotBrain.requestWatering(0);
 }
 
 void loop()
 {
 }
 
+class RobotBrain {
+  public:
+  RobotBrain(Pump[] pumpList, unsigned long[] durationList) {
+    // tests
+    TEST_Pump(7);
+    TEST_Sensor(8, A0);
+  }
+  
+  /**
+   * @brief (a plant) requests the robot brain to water a plant with the given id (all neccessary steps before and after watering - like moving - are performed)
+   * 
+   * @param pump the pump that should water the plant
+   * @param duration the duration of the watering in milliseconds
+   */
+  void requestWatering(Pump pump, unsigned long duration) {
+    // move the pump to plant
+    pump.pumpForTime(5000);
+  }
+  
+  
+/**
+ * @brief a test function for the pump class
+ * 
+ * @param pumpPin the pin number of the pump to test
+ */ 
 void TEST_Pump(int pumpPin) {
     Pump testPump = Pump(pumpPin);
     testPump.pumpForTime(5000);
 }
 
+/**
+ * @brief a test function for the sensor class
+ * 
+ * @param powerPin the pin number of the power supply for the sensor (?)
+ * @param sensorPin the pin number of the sensor
+ */
 void TEST_Sensor(int powerPin, int sensorPin) {
   Sensor testSensor = Sensor(powerPin, sensorPin);
 
