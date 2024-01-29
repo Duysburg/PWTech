f5c3a15095960dcd0fe168e6c2b6c75a2ce75f73
Happy <connect1234567890@web.de>
2024-01-29 15:52:46 +0100

    added blinkLed() function

--- RobotBrain/PlantUnit.h
+++ RobotBrain/PlantUnit.h
@@ -13,13 +13,32 @@ private:
 
     /**
      * @brief This function checks the measured moisture value from the sensor and returns a boolean value based on whether the measured value is within the acceptable range.
+     * (The inbuild LED is blinking if the measured moisture value is below the acceptable range.)
      * 
      * @return true if the measured moisture value is within the acceptable range, false otherwise.
      */
     bool checkSensor() {
         int measuredMoisture = sensor.measureMoisture();
-        return (measuredMoisture >= optimalMoisture - moistureThreshhold && measuredMoisture <= optimalMoisture + moistureThreshhold);
+        bool isAcceptable = (measuredMoisture >= optimalMoisture - moistureThreshhold && measuredMoisture <= optimalMoisture + moistureThreshhold);
+        if (!isAcceptable) {
+          blinkLed(100);
+        }
+        return isAcceptable;
     }
+    
+      /**
+     * @brief this function blinks the built-in LED on the Arduino board for a specified duration
+     * 
+     * @param durationInMillis The duration for which the LED is on or off (in milliseconds)
+     */
+    void blinkLed(long durationInMillis) {
+        for (int i = 0; i < 5; i++) {
+          digitalWrite(LED_BUILTIN, HIGH);
+          delay(durationInMillis);
+          digitalWrite(LED_BUILTIN, LOW);
+          delay(durationInMillis);
+        }
+      }
 
 public:
     bool currentlyBeeingWatered = false;
