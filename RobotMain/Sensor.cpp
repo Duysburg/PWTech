f2b33e9f3d1b46f7f2ecb4654ecd2a5fdce3a8f7
Happy <connect1234567890@web.de>
2024-02-01 09:00:30 +0100

    documentation

--- RobotMain/Sensor.cpp
+++ RobotMain/Sensor.cpp
@@ -1,13 +1,19 @@
 #include "Sensor.h"
 
+/**
+ * @brief Construct a new Sensor object
+ * 
+ * @param readingPin the pin on the Arduino board connected to the moisture sensor
+ */
 Sensor::Sensor(int readingPin)
 {
     this->readingPin = readingPin;
 }
 
 /**
- * @brief measure the current moisture level
- * @return current moisture level
+ * @brief Measure the current moisture level
+ * 
+ * @return the current moisture level
  */
 int Sensor::measureMoisture()
 {
